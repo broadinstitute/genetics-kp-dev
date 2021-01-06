3976c298317d28065641ef67088324c772d4e6a9
Marc Duby <mduby@broadinstitute.org>
2021-01-06 11:42:45 -0500

    API: added manually modified files

--- python-flask-server/openapi_server/controllers/predicates_controller.py
+++ python-flask-server/openapi_server/controllers/predicates_controller.py
@@ -12,26 +12,4 @@ def predicates_get():  # noqa: E501
 
     :rtype: Dict[str, Dict[str, List[str]]]
     """
-    predicates = {}
-    predicates['disease'] = {}
-    predicates['disease']['gene'] = []
-    predicates['disease']['gene'].append('associated')
-    predicates['disease']['pathway'] = []
-    predicates['disease']['pathway'].append('associated')
-    predicates['phenotypic_feature'] = {}
-    predicates['phenotypic_feature']['gene'] = []
-    predicates['phenotypic_feature']['gene'].append('associated')
-    predicates['phenotypic_feature']['pathway'] = []
-    predicates['phenotypic_feature']['pathway'].append('associated')
-    predicates['gene'] = {}
-    predicates['gene']['disease'] = []
-    predicates['gene']['disease'].append('associated')
-    predicates['gene']['phenotypic_feature'] = []
-    predicates['gene']['phenotypic_feature'].append('associated')
-    predicates['pathway'] = {}
-    predicates['pathway']['disease'] = []
-    predicates['pathway']['disease'].append('associated')
-    predicates['pathway']['phenotypic_feature'] = []
-    predicates['pathway']['phenotypic_feature'].append('associated')
-
-    return predicates
+    return 'do some magic!'
