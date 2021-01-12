a40966d757c10c1dedf53cc12e911eb881ecab48
Marc Duby <mduby@broadinstitute.org>
2021-01-12 14:18:57 -0500

    trapi1.0: fixed typo

--- python-flask-server/openapi_server/controllers/query_controller.py
+++ python-flask-server/openapi_server/controllers/query_controller.py
@@ -266,8 +266,8 @@ def query(request_body):  # noqa: E501
     if connexion.request.is_json:
         # initialize
         # cnx = mysql.connector.connect(database='Translator', user='mvon')
-        # cnx = pymysql.connect(host='localhost', port=3306, database='Translator', user='mvon')
-        cnx = pymysql.connect(host='localhost', port=3306, database='tran_genepro', user='root', password='yoyoma')
+        cnx = pymysql.connect(host='localhost', port=3306, database='Translator', user='mvon')
+        # cnx = pymysql.connect(host='localhost', port=3306, database='tran_genepro', user='root', password='this is no password')
         cursor = cnx.cursor()
         genetics_results = []
         query_response = {}
