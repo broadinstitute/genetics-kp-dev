eb5c5292d01c6367b8e0bb38f7494bc540fa7f6c
Marc Duby <mduby@broadinstitute.org>
2021-01-12 10:41:03 -0500

    trapi1.0: code cleanup

--- python-flask-server/openapi_server/controllers/query_controller.py
+++ python-flask-server/openapi_server/controllers/query_controller.py
@@ -307,7 +307,6 @@ def query(request_body):  # noqa: E501
 
             # queries
             if (sourceType == 'disease' or sourceType == 'phenotypic_feature') and targetType == 'gene':
-                # N = 2
                 info = [["MAGMA-pvalue", "smaller_is_better"],\
                         ["Richards-effector-genes", "higher_is_better"],\
                         ["ABC-genes", "not_displayed"],\
@@ -329,14 +328,12 @@ def query(request_body):  # noqa: E501
                                 mg.DISEASE='{}' and mg.CATEGORY='{}' and mg.SCORE >0.95   ORDER by mg.SCORE  DESC""".format(sourceID,sourceType)]
 
             elif (sourceType == 'disease' or sourceType == 'phenotypic_feature') and targetType == 'pathway':
-                # N = 1
                 info = [["MAGMA-pvalue", "smaller_is_better"]]
                 queries = ["""select mp.PATHWAY, mp.ID, mp.PVALUE, pl.efo_name, null from MAGMA_PATHWAYS mp, phenotype_lookup pl 
                         where mp.DISEASE = pl.tran_efo_id and 
                         mp.DISEASE='{}' and mp.CATEGORY='{}' and mp.PVALUE<2.0e-6 ORDER by mp.PVALUE ASC""".format(sourceID,sourceType)]
 
             elif sourceType == 'gene' and (targetType == 'disease' or targetType == 'phenotypic_feature'):
-                # N = 2
                 info = [["MAGMA-pvalue", "smaller_is_better"],\
                         ["Richards-effector-genes", "higher_is_better"],\
                         ["ABC-genes", "not_displayed"],\
@@ -358,7 +355,6 @@ def query(request_body):  # noqa: E501
                                 mg.GENE='{}' and mg.CATEGORY='{}' and mg.SCORE >0.80 ORDER by SCORE DESC""".format(sourceID,targetType)]
 
             elif sourceType == 'pathway' and (targetType == 'disease' or targetType == 'phenotypic_feature'):
-                # N = 1
                 info = [["MAGMA-pvalue", "smaller_is_better"]]
                 queries = ["""select mp.DISEASE, mp.ID, mp.PVALUE, null, pl.efo_name from MAGMA_PATHWAYS  mp, phenotype_lookup pl 
                         where mp.DISEASE = pl.tran_efo_id and 
@@ -386,26 +382,7 @@ def query(request_body):  # noqa: E501
                             # add to the results list
                             genetics_results.append(output_edge)
 
-        #                     if sourceID not in takenNodes:
-        #                         body['knowledge_graph']['nodes'].append({"id" : sourceID, "type" : sourceType})
-        #                         takenNodes[sourceID] = 1
-
-        #                     if targetID not in takenNodes:
-        #                         body['knowledge_graph']['nodes'].append({"id" : targetID, "type" : targetType})
-        #                         takenNodes[targetID] = 1
-
-        #                     if edgeID not in takenEdges: 
-        #                         if score is not None:
-        #                             body['knowledge_graph']['edges'].append({"id" : edgeID, "source_id": sourceID, "target_id" : targetID, "score_name" : info[i][0], "score" : score, "score_direction" : info[i][1], "type" : "associated"})
-        #                         else:
-        #                             body['knowledge_graph']['edges'].append({"id" : edgeID, "source_id": sourceID, "target_id" : targetID, "score_name" : info[i][0], "type" : "associated"})
-        #                         takenEdges[edgeID] = 1
-
-        #                     body['results'].append({"edge_bindings": [ {"kg_id": edgeID, "qg_id": qeID} ], "node_bindings": [ { "kg_id": sourceID, "qg_id": qn0ID }, { "kg_id": targetID, 'qg_id': qn1ID } ] })
-
-        # body['query_graph'] = body['message']['query_graph']
-        # del body['message']
-        # return body
+        # close the connection
         cnx.close()
 
         # build the response
@@ -413,6 +390,7 @@ def query(request_body):  # noqa: E501
         return query_response
 
     else :
+        # return error
         return({"status": 400, "title": "body content not JSON", "detail": "Required body content is not JSON", "type": "about:blank"}, 400)
 
 def get_data(edge, sourceNode, targetNode):
