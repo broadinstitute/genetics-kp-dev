d73bed757f0d7006205c2d150cb40807ca23419e
Marc Duby <mduby@broadinstitute.org>
2021-08-19 09:28:18 -0400

    perform: added gunicorn

--- python-flask-server/openapi_server/__main__.py
+++ python-flask-server/openapi_server/__main__.py
@@ -4,19 +4,30 @@ import connexion
 import os
 from openapi_server import encoder
 
+# def main():
+#     app = connexion.App(__name__, specification_dir='./openapi/')
+#     app.app.json_encoder = encoder.JSONEncoder
+#     app.add_api('openapi.yaml',
+#                 arguments={'title': 'Genetics Data Provider for NCATS Biomedical Translator Reasoners'},
+#                 pythonic_params=True)
+
+#     # config
+#     network_port = os.environ.get('PORT')
+
+#     # app.run(port=8080)
+#     # app.run(port=7002)
+#     app.run(port=network_port)
+
+
+app = connexion.App(__name__, specification_dir='./openapi/')
+app.app.json_encoder = encoder.JSONEncoder
+app.add_api('openapi.yaml',
+            arguments={'title': 'Molecular Data Provider for NCATS Biomedical Translator'},
+            pythonic_params=True)
 
 def main():
-    app = connexion.App(__name__, specification_dir='./openapi/')
-    app.app.json_encoder = encoder.JSONEncoder
-    app.add_api('openapi.yaml',
-                arguments={'title': 'Genetics Data Provider for NCATS Biomedical Translator Reasoners'},
-                pythonic_params=True)
-
     # config
     network_port = os.environ.get('PORT')
-
-    # app.run(port=8080)
-    # app.run(port=7002)
     app.run(port=network_port)
 
 
