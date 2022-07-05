d4fdc871ef97977936e0e65c92ad57859bec1c74
Marc Duby <mduby@broadinstitute.org>
2022-07-04 23:36:08 -0400

    creative: fixed query bug

--- python-flask-server/openapi_server/dcc/query_builder.py
+++ python-flask-server/openapi_server/dcc/query_builder.py
@@ -449,6 +449,29 @@ def build_creative_query(web_query_object, log=False):
         order by path_disease.score, gene_disease.score
         '''
 
+    sql_string = '''
+select concat(path_disease.id, '_', gene_disease.id, '_', drug_gene.id, '_', pathway_gene.id) as result_id,
+    pathway.ontology_id as pathway, pathway.node_name as pathway_name, 
+    gene.ontology_id as gene, gene.node_name as gene_name,
+    disease.ontology_id as disease, disease.node_name as disease_name,
+    path_disease.score as pathway_score, gene_disease.score as gene_score, 
+    drug_gene.drug_ontology_id as drug, drug_gene.drug_name, drug_gene.drug_category_biolink_id as drug_category,
+    drug_gene.predicate_biolink_id as gene_drug_predicate,
+    drug_gene.id as drug_gene_row_id, gene_disease.id as gene_disease_row_id, 
+    pathway_gene.id as path_gene_row_id, path_disease.id as path_disease_row_id
+from comb_edge_node path_disease, comb_edge_node gene_disease, 
+    comb_node_ontology pathway, comb_node_ontology gene,
+    comb_node_ontology disease,
+    comb_pathway_gene pathway_gene,
+    infe_drug_gene drug_gene
+where path_disease.source_node_id = pathway.id and path_disease.target_node_id = disease.id 
+    and gene_disease.source_node_id = gene.id and gene_disease.target_node_id = disease.id
+    and pathway.node_type_id = 4 and gene.node_type_id = 2 and disease.node_type_id = 1
+    and path_disease.score < 0.005 and (gene_disease.score < 0.000006 or gene_disease.score_translator > 0.1)
+    and pathway_gene.gene_node_id = gene.id and pathway_gene.pathway_node_id = pathway.id
+    and gene.ontology_id = drug_gene.gene_ontology_id
+    '''
+
     if web_query_object.get_list_source_id():
         list_input = web_query_object.get_list_source_id()
         sql_string = add_in_in(sql=sql_string, term="gene.ontology_id", list_input=list_input, is_first=False)
@@ -458,6 +481,8 @@ def build_creative_query(web_query_object, log=False):
         list_input = web_query_object.get_list_target_id()
         sql_string = add_in_in(sql=sql_string, term="disease.ontology_id", list_input=list_input, is_first=False)
         param_list += list_input
+
+    sql_string = sql_string + ' order by path_disease.score, gene_disease.score '
 
     sql_string = sql_string + " limit " + str(500)
 
