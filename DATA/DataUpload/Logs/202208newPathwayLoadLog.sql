a3789a8a613cc7928bce2a413b2ff1c521b814e9
Marc Duby <mduby@broadinstitute.org>
2022-08-23 20:50:38 -0400

    data: adding new 202206 aggregtor phenotypes

--- DATA/DataUpload/Logs/202208newPathwayLoadLog.sql
+++ DATA/DataUpload/Logs/202208newPathwayLoadLog.sql
@@ -1,6 +1,7 @@
 
 
 -- steps taken
+-- 0 - add new phenotypes
 -- 1 - add new ontologies to comb_ontology_type table -> see lookupTables.sql
 -- 2 - loaded new pathways that were not in the comb_node_ontology tables already -> see loadUpkeepPathwayToTranslator.sql
 -- TODO 3 - load pathway genes
@@ -15,7 +16,27 @@ and a.node_type_id = c.type_id
 group by b.ontology_name, c.type_name
 order by b.ontology_name, c.type_name;
 
--- pre pathway load
+-- post new pathway load
+
+
+
+-- post new phenotype load
+-- +-------------+-------------------------+---------------------------+
+-- | count(a.id) | ontology_name           | type_name                 |
+-- +-------------+-------------------------+---------------------------+
+-- |           7 | EFO disease/phenotype   | biolink:Disease           |
+-- |         152 | EFO disease/phenotype   | biolink:PhenotypicFeature |
+-- |        7279 | GO pathway              | biolink:Pathway           |
+-- |          19 | HP disease/phenotype    | biolink:PhenotypicFeature |
+-- |        5702 | MONDO disease/phenotype | biolink:Disease           |
+-- |           2 | MONDO disease/phenotype | biolink:PhenotypicFeature |
+-- |       19500 | NCBI Gene               | biolink:Gene              |
+-- |           1 | NCIT disease/phenotype  | biolink:PhenotypicFeature |
+-- |           5 | UMLS disease/phenotype  | biolink:PhenotypicFeature |
+-- +-------------+-------------------------+---------------------------+
+-- 9 rows in set (0.11 sec)
+
+-- pre new phenotype load
 -- +-------------+-------------------------+---------------------------+
 -- | count(a.id) | ontology_name           | type_name                 |
 -- +-------------+-------------------------+---------------------------+
---
+++ DATA/DataUpload/Process/Phenotypes/loadUpkeepPhenotypeToTranslator.sql
@@ -0,0 +1,82 @@
+
+-- create table
+drop table if exists agg_aggregator_phenotype;
+create table agg_aggregator_phenotype (
+  id                        int not null auto_increment primary key,
+  phenotype_id              varchar(100) not null,
+  phenotype_name            varchar(500),
+  group_name                varchar(500),
+  ontology_id               varchar(100),
+  in_translator             enum('true', 'false') default 'false',
+  just_added_in             enum('true', 'false') default 'false',
+  last_updated              timestamp default CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+alter table agg_aggregator_phenotype add unique index u_phenotype_id_idx (phenotype_id);
+
+
+
+
+-- insert new phenotypes into the comb_node_ontology table
+insert into tran_test_202209.comb_node_ontology
+(node_code, node_type_id, ontology_id, ontology_type_id, node_name, added_by_study_id)
+select up_phenotype.phenotype_id, 
+(case when SUBSTRING_INDEX(SUBSTRING_INDEX(up_phenotype.ontology_id, ':', 1), ':', -1) = 'MONDO' then 1 else 3 end) as node_type,
+up_phenotype.ontology_id, ont_type.ontology_id, up_phenotype.phenotype_name, 1
+from tran_upkeep.agg_aggregator_phenotype up_phenotype, tran_test_202209.comb_ontology_type ont_type 
+where up_phenotype.ontology_id is not null
+and SUBSTRING_INDEX(SUBSTRING_INDEX(up_phenotype.ontology_id, ':', 1), ':', -1) collate utf8mb4_unicode_ci = ont_type.prefix
+and up_phenotype.phenotype_id collate utf8mb4_unicode_ci not in (select node_code from tran_test_202209.comb_node_ontology where node_type_id in (1, 3))
+and up_phenotype.id not in (479, 480);
+
+-- select up_phenotype.id,  up_phenotype.phenotype_id, 
+
+
+
+-- workflow
+update tran_upkeep.agg_aggregator_phenotype set in_translator = 'true'
+where phenotype_id COLLATE utf8mb4_general_ci in (
+  select node_code from tran_test_202208.comb_node_ontology where node_type_id in (1, 3)
+);
+
+
+
+-- one off updates
+-- add magma as the source study for previous phenotypes
+update tran_test_202209.comb_node_ontology
+set added_by_study_id = 1
+where node_type_id in (1, 3)
+and node_code in (select phenotype_id collate utf8mb4_unicode_ci from tran_upkeep.agg_aggregator_phenotype);
+
+select id, node_code, node_name from tran_test_202209.comb_node_ontology
+where node_type_id in (1, 3)
+and node_code in (select phenotype_id collate utf8mb4_unicode_ci from tran_upkeep.agg_aggregator_phenotype);
+
+
+
+-- debug
+select count(id) from agg_aggregator_phenotype;
+
+select count(id), in_translator 
+from agg_aggregator_phenotype
+where ontology_id is not null
+group by in_translator;
+
+select * 
+from tran_upkeep.agg_aggregator_phenotype
+where ontology_id is not null
+and in_translator = 'false'
+order by phenotype_name;
+
+
+select id, phenotype_id, phenotype_name, ontology_id, in_translator, just_added_in
+from tran_upkeep.agg_aggregator_phenotype
+where ontology_id is null and in_translator = 'false'
+order by phenotype_name;
+
+
+select id, substring(node_code, 1, 20), node_type_id, ontology_id, substring(node_name, 1, 20), added_by_study_id
+from tran_test_202209.comb_node_ontology 
+where node_type_id in (1, 3) and added_by_study_id = 1
+order by node_name, ontology_id;
+
