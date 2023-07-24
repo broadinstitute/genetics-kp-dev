fed676f2c8c9499e34a9a9034f693831975bca6d
Marc Duby <mduby@broadinstitute.org>
2023-07-24 11:23:14 -0400

    test: fixed itrb prod test after deployment

--- python-flask-server/tests/cloud/test_itrb_prod.py
+++ python-flask-server/tests/cloud/test_itrb_prod.py
@@ -3,7 +3,7 @@
 import openapi_server.dcc.result_utils as rutils
 
 # constants
-url_trapi_service = "https://genetics-kp.transltr.io/genetics_provider/trapi/v1.3/{}"
+url_trapi_service = "https://genetics-kp.transltr.io/genetics_provider/trapi/v1.4/{}"
 
 # tests
 def test_query_api():
