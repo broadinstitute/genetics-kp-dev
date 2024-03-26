af899b5b88efc3948fa87b41a1e1c901e68bf914
Marc Duby <mduby@broadinstitute.org>
2024-03-26 11:21:07 -0400

    trapi: modified generated controllers

--- python-flask-server/openapi_server/controllers/meta_knowledge_graph_controller.py
+++ python-flask-server/openapi_server/controllers/meta_knowledge_graph_controller.py
@@ -7,10 +7,12 @@ from typing import Union
 from openapi_server.models.meta_knowledge_graph import MetaKnowledgeGraph  # noqa: E501
 from openapi_server import util
 
+# geneticspro specific imports
 from openapi_server.models.meta_node import MetaNode  # noqa: E501
 from openapi_server.models.meta_edge import MetaEdge  # noqa: E501
-
 from openapi_server.dcc.biolink_utils import create_predicate_triple_list, get_node_map
+
+
 
 # def meta_knowledge_graph_get():  # noqa: E501
 #     """Meta knowledge graph representation of this TRAPI web service.
@@ -23,10 +25,11 @@ from openapi_server.dcc.biolink_utils import create_predicate_triple_list, get_n
 #     return 'do some magic!'
 
 
+
 def meta_knowledge_graph_get():  # noqa: E501
     """Meta knowledge graph representation of this TRAPI web service.
      # noqa: E501
-    :rtype: MetaKnowledgeGraph
+    :rtype: Union[MetaKnowledgeGraph, Tuple[MetaKnowledgeGraph, int], Tuple[MetaKnowledgeGraph, int, Dict[str, str]]
     """
     # return 'do some magic!'
 
--- python-flask-server/openapi_server/controllers/query_controller.py
+++ python-flask-server/openapi_server/controllers/query_controller.py
@@ -7,6 +7,8 @@ from typing import Union
 from openapi_server.models.query import Query  # noqa: E501
 from openapi_server.models.response import Response  # noqa: E501
 from openapi_server import util
+
+# geneticspro specific imports
 from openapi_server.dcc.web_utils import query
 
 
@@ -22,16 +24,16 @@ from openapi_server.dcc.web_utils import query
 #     """
 #     return 'do some magic!'
 
-
 def query_post(request_body):  # noqa: E501
     """Query reasoner via one of several inputs
      # noqa: E501
     :param request_body: Query information to be submitted
     :type request_body: Dict[str, ]
-    :rtype: Response
+    :rtype: Union[Response, Tuple[Response, int], Tuple[Response, int, Dict[str, str]]
     """
     # return 'do some magic!'
 
     # get the response
     response = query(request_body)
     return response
+
