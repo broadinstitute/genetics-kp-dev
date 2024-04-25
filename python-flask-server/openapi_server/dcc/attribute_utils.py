1a92a21eb89e137bedfe7279219aa35e2dcda4d1
Marc Duby <mduby@broadinstitute.org>
2024-04-25 16:31:08 -0400

    trapi: added attribute building method for score/publications

---
+++ python-flask-server/openapi_server/dcc/attribute_utils.py
@@ -0,0 +1,44 @@
+
+'''
+file to hold utils to help build resutl attributes
+'''
+
+
+# imports
+from openapi_server.models.attribute import Attribute
+
+
+# constants
+BIOLINK_SCORE = 'biolink:score'
+BIOLINK_BETA = 'biolink:beta'
+BIOLINK_PVALUE = 'biolink:p-value'
+BIOLINK_STANDARD_ERROR = 'biolink:standard_error'
+BIOLINK_PROBABILITY = 'biolink:probability'
+BIOLINK_CLASSIFICATION = 'biolink:classification'
+BIOLINK_PUBLICATION = 'biolink:publications'
+BIOLINK_SUPPORT_GRAPH = 'biolink:support_graphs'
+BIOLINK_KNOWLEDGE_LEVEL = 'biolink:knowledge_level'
+BIOLINK_AGENT_TYPE = 'biolink:agent_type'
+
+TYPE_VALUE_STRING = 'String'
+TYPE_VALUE_DOUBLE = 'Double'
+TYPE_VALUE_PUBLICATIONS = 'linkml:Uriorcurie'
+
+
+KNOWLEDGE_STATS = 'statistical_association'
+KNOWLEDGE_PREDICTION = 'prediction'
+
+AGENT_COMPUTATION = "computational_model"
+AGENT_PIPELINE = "data_analysis_pipeline"
+
+INFORES_ROOT = "infores:cmap"
+
+NAME_KNOWLEDGE_LEVEL = 'knowledge level'
+NAME_AGENT_TYPE = 'agent type'
+NAME_PUBLICATIONS = 'publications'
+
+# methods
+
+
+
+
