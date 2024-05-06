ca18bfdd0be3d1885157903e9a04118b2063e23d
Marc Duby <mduby@broadinstitute.org>
2024-05-06 10:03:00 -0400

    test: fixed itrb cloud integration test

--- python-flask-server/tests/cloud/test_itrb_test.py
+++ python-flask-server/tests/cloud/test_itrb_test.py
@@ -25,7 +25,7 @@ def test_creative_query_api():
     url = url_trapi_service.format("query")
 
     # call the query service and get the nodes
-    map_nodes = rutils.post_query_nodes_one_hop(url, None, ["MONDO:0004975"], ["biolink:ChemicalEntity"], ["biolink:Disease"], ["biolink:treats"], knowledge_type="inferred", log=False)
+    map_nodes = rutils.post_query_nodes_one_hop(url, None, ["MONDO:0004975"], ["biolink:ChemicalEntity"], ["biolink:Disease"], ["biolink:affects"], knowledge_type="inferred", log=False)
 
     # test
     assert len(map_nodes) > 0
