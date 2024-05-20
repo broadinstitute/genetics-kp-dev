a31bac28fb5bd2a020631f9374b10aefc0296136
Marc Duby <mduby@broadinstitute.org>
2024-05-20 10:53:42 -0400

    trapi: updated integration test for resting to 1.5

--- python-flask-server/tests/cloud/test_itrb_test.py
+++ python-flask-server/tests/cloud/test_itrb_test.py
@@ -3,7 +3,7 @@
 import openapi_server.dcc.result_utils as rutils
 
 # constants
-url_trapi_service = "https://genetics-kp.test.transltr.io/genetics_provider/trapi/v1.4/{}"
+url_trapi_service = "https://genetics-kp.test.transltr.io/genetics_provider/trapi/v1.5/{}"
 
 # tests
 def test_query_api():
