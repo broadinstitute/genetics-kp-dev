b46cdf72f487f7c30a5172ea6bb960f66140a17c
Marc Duby <mduby@broadinstitute.org>
2024-05-20 14:35:37 -0400

    mcq: trapi working with dummay data

--- python-flask-server/openapi_server/dcc/multi_curie_utils.py
+++ python-flask-server/openapi_server/dcc/multi_curie_utils.py
@@ -1,15 +1,24 @@
 
 # imports
+import sqlite3
+
 from openapi_server.models.message import Message
 from openapi_server.models.query import Query
 from openapi_server.models.query_graph import QueryGraph
 from openapi_server.models.response import Response
+from openapi_server.models.edge import Edge
+from openapi_server.models.node import Node
 
-from openapi_server.dcc.trapi_utils import build_results, build_results_creative14, get_biolink_version, get_trapi_version
+import openapi_server.dcc.trapi_utils as tutils
 import openapi_server.dcc.trapi_constants as trapi_constants
 
+from openapi_server.dcc.utils import get_logger
+
+# logger
+logger = get_logger('multi_curie_utils.py')
+
 # constants
-FILE_DB = ""
+FILE_DB = "conf/mcq.db"
 
 DB_QUERY_GENE_PHENOTYPE = """
 select gene_pheno.gene, pheno.name as phenotype, pheno.query_ontology_id as ontology_id, gene_pheno.probability
@@ -17,7 +26,7 @@ from mcq_phenotype pheno, mcq_gene_phenotype gene_pheno
 where gene_pheno.phenotype = pheno.name 
 and pheno.query_ontology_id in ({})
 order by gene_pheno.probability desc 
-limit 20;
+limit 20
 """
 
 
@@ -30,22 +39,27 @@ def db_query_phenotype(conn, list_phenotypes, log=False):
     list_result = []
     cursor = conn.cursor()
 
-    # Create a placeholder string for the number of values
+    # build the placeholders
     placeholders = ', '.join('?' for _ in list_phenotypes)
 
     # Construct the query
     query = DB_QUERY_GENE_PHENOTYPE.format(placeholders)
+
+    # Execute the query with the provided values
+    logger.info("running query: {} for inputs: {}".format(query, list_phenotypes))
 
     # query
     cursor.execute(query, list_phenotypes)
 
     # Fetch all matching rows
     rows = cursor.fetchall()
+    logger.info("got result tows of count: {}".format(len(rows)))
 
     # get the data
+    # list_result = [dict(row) for row in rows]
     for row in rows:
-        map_row = dict(row)
-        list_result.append(map_row)
+        list_result.append({'gene_name': row[0], 'phenotype_name': row[1], 'phenotype_id': row[2], 'probability': row[3]})
+    logger.info("got DB results: {}".format(list_result))
 
     # return
     return list_result
@@ -56,23 +70,98 @@ def sub_query_mcq(trapi_query: Query, log=False):
     respond to a trapi query
     '''
     # initialize 
-    logs = ["query is lookup", "query is MANY muti curie"]
-    trapi_respponse = Response(message=trapi_query.message, logs=logs, workflow=trapi_query.workflow, 
-                            biolink_version=get_biolink_version(), schema_version=get_trapi_version())
-
+    list_logs = ["query is lookup", "query is MANY muti curie"]
+    trapi_respponse = Response(message=trapi_query.message, logs=list_logs, workflow=trapi_query.workflow, 
+                            biolink_version=tutils.get_biolink_version(), schema_version=tutils.get_trapi_version())
+    list_mcq_nodes = []
+    map_nodes = {}
+    map_edges = {}
 
     # get the inputs
+    if trapi_query:
+        message: Message = trapi_query.message
+        Message.results = []
+        if message.query_graph:
+            query_graph: QueryGraph = message.query_graph
+            if query_graph.nodes and len(query_graph.nodes) > 0:
+                for node in query_graph.nodes.values():
+                    set_interpretation = node.set_interpretation
+                    if set_interpretation in [trapi_constants.SET_INTERPRETATION_ALL, trapi_constants.SET_INTERPRETATION_MANY]:
+                        input_set_interpretation = set_interpretation
+                        if node.ids:
+                            # make sure at least 1 element
+                            list_mcq_nodes = node.ids
+                            if len(list_mcq_nodes) < 1:
+                                log_msg = "Error: no curies provided for set interpretation: {}".format(input_set_interpretation)
+                                list_logs.append(log_msg)
+                            else:
+                                # add acceptance log message
+                                log_msg = "processing mcq query with set interpretation {} for nodes: {}".format(input_set_interpretation, list_mcq_nodes)
+                                list_logs.append(log_msg)
+                                
+                        else:
+                            log_msg = "Error: no curies provided for set interpretation: {}".format(input_set_interpretation)
+                            list_logs.append(log_msg)
 
     
-    # get the data
+    # get the db connection
+    conn = sqlite3.connect(FILE_DB)
 
+    # get the data
+    list_genes = db_query_phenotype(conn=conn, list_phenotypes=list_mcq_nodes)
+
+    # calculate the data
+    # will get a gene -> score map
+    list_result = calculate_from_results(list_genes=list_genes)
 
     # build the response
+    # build object set node
+    node_object : Node = tutils.build_node_knowledge_graph(ontology_id='trapi:set01', name='trapi:set01', list_categories=[trapi_constants.BIOLINK_ENTITY_PHENOTYPE])
+    map_nodes[node_object.name] = node_object
+    for row in list_result:
+        # build the score attribute
+        list_attributes = [tutils.build_attribute(list(row.values())[0], trapi_constants.BIOLINK_SCORE, id_source=trapi_constants.PROVENANCE_INFORES_KP_GENETICS)]
 
+        # build subject gene node
+        name_gene = list(row.keys())[0]
+        node_subject: Node = tutils.build_node_knowledge_graph(ontology_id=name_gene, name=name_gene, list_categories=[trapi_constants.BIOLINK_ENTITY_GENE])
+
+        # buid the edge
+        key_edge, edge = tutils.build_edge_knowledge_graph(predicate=trapi_constants.BIOLINK_PREDICATE_GENETIC_ASSOCIATION, key_subject=node_subject.name, key_object=node_object.name, list_attributes=list_attributes)
+
+        # add the nodes, edge to the map
+        map_nodes[name_gene] = node_subject
+        map_edges[key_edge] = edge
+        
+    # build the KG and add to response
+    trapi_query.message.knowledge_graph = tutils.build_knowledge_graph(map_edges=map_edges, map_nodes=map_nodes, log=True)
 
     # return
+    trapi_respponse.logs = list_logs
     return trapi_respponse
 
+
+
+def calculate_from_results(list_genes, log=False):
+    '''
+    will do the algorithmic calculation of the results 
+    '''
+    # initialize
+    map_phenotype_weight = {}
+    list_result = [{'PPARG': 75}, {'SLC30A8': 90}, {'PCSK9': 58}]
+
+    # build the phenotype weight
+
+    # for each result, build the gene probability
+
+    # build list of maps, with gene as key
+
+    # sort list
+    list_sorted_result = sorted(list_result, key=lambda item: list(item.values())[0], reverse=True)
+    logger.info("got sorted list: {}".format(list_sorted_result))
+
+    # return
+    return list_sorted_result
 
 def query_multi_curie(query: Query, log=False):
     ''' 
@@ -116,7 +205,7 @@ def query_multi_curie(query: Query, log=False):
 
     # return
     return Response(message=query.message, logs=logs, workflow=query.workflow, 
-                biolink_version=get_biolink_version(), schema_version=get_trapi_version())
+                biolink_version=tutils.get_biolink_version(), schema_version=tuple.get_trapi_version())
 
 
 # main
--- python-flask-server/openapi_server/dcc/trapi_constants.py
+++ python-flask-server/openapi_server/dcc/trapi_constants.py
@@ -8,3 +8,68 @@ OPERATION_LOOKUP = "lookup"
 
 KNOWLEDGE_TYPE_INFERRED = "inferred"
 
+
+# biolink
+BIOLINK_ENTITY_GENE = 'biolink:Gene'
+BIOLINK_ENTITY_SET = 'biolink:Set'
+BIOLINK_ENTITY_DISEASE = 'biolink:Disease'
+BIOLINK_ENTITY_PHENOTYPE = 'biolink:Phenotype'
+
+BIOLINK_PREDICATE_GENETIC_ASSOCIATION = 'biolink:genetic_association'
+
+
+# constants
+BIOLINK_SCORE = 'biolink:score'
+BIOLINK_BETA = 'biolink:beta'
+BIOLINK_PVALUE = 'biolink:p-value'
+BIOLINK_STANDARD_ERROR = 'biolink:standard_error'
+BIOLINK_PROBABILITY = 'biolink:probability'
+BIOLINK_CLASSIFICATION = 'biolink:classification'
+BIOLINK_PUBLICATION = 'biolink:publications'
+BIOLINK_SUPPORT_GRAPH = 'biolink:support_graphs'
+BIOLINK_KNOWLEDGE_LEVEL = 'biolink:knowledge_level'
+BIOLINK_AGENT_TYPE = 'biolink:agent_type'
+
+TYPE_VALUE_STRING = 'String'
+TYPE_VALUE_DOUBLE = 'Double'
+TYPE_VALUE_PUBLICATIONS = 'linkml:Uriorcurie'
+
+
+KNOWLEDGE_STATS = 'statistical_association'
+KNOWLEDGE_PREDICTION = 'prediction'
+
+AGENT_COMPUTATION = "computational_model"
+AGENT_PIPELINE = "data_analysis_pipeline"
+
+INFORES_ROOT = "infores:cmap"
+
+NAME_KNOWLEDGE_LEVEL = 'knowledge level'
+NAME_AGENT_TYPE = 'agent type'
+NAME_PUBLICATIONS = 'publications'
+NAME_SCORE = 'score'
+NAME_BETA = 'beta'
+NAME_PVALUE = 'p-value'
+NAME_STANDARD_ERROR = 'standard error'
+NAME_PROBABILITY = 'probability'
+NAME_CLASSIFICATION = 'classification'
+
+
+MAP_NAME_ATTRIBUTE = {
+    BIOLINK_SCORE: NAME_SCORE,
+    BIOLINK_BETA: NAME_BETA,
+    BIOLINK_PVALUE: NAME_PVALUE,
+    BIOLINK_STANDARD_ERROR: NAME_STANDARD_ERROR, 
+    BIOLINK_PROBABILITY: NAME_PROBABILITY,
+    BIOLINK_CLASSIFICATION: NAME_CLASSIFICATION,
+    BIOLINK_PUBLICATION: NAME_PUBLICATIONS
+}
+
+# provenance
+PROVENANCE_INFORES_KP_GENETICS='infores:genetics-data-provider'
+PROVENANCE_INFORES_KP_MOLEPRO='infores:molepro'
+PROVENANCE_INFORES_CLINVAR='infores:clinvar'
+PROVENANCE_INFORES_CLINGEN='infores:clingen'
+PROVENANCE_INFORES_GENCC='infores:gencc'
+PROVENANCE_INFORES_GENEBASS='infores:genebass'
+PROVENANCE_INFORES_RICHARDS='infores:regl'
+
