990a2c42603ad223d3e86cad35dcc0fd3e37e00d
Marc Duby <mduby@broadinstitute.org>
2024-05-23 00:14:12 -0400

    gpt: added query graph extractor

--- python-flask-server/openapi_server/dcc/trapi_utils.py
+++ python-flask-server/openapi_server/dcc/trapi_utils.py
@@ -11,6 +11,10 @@ from openapi_server.models.message import Message
 from openapi_server.models.knowledge_graph import KnowledgeGraph
 from openapi_server.models.edge import Edge
 from openapi_server.models.node import Node
+from openapi_server.models.query import Query
+from openapi_server.models.q_edge import QEdge
+from openapi_server.models.q_node import QNode
+from openapi_server.models.query_graph import QueryGraph
 from openapi_server.models.qualifier import Qualifier
 from openapi_server.models.result import Result
 from openapi_server.models.edge_binding import EdgeBinding
@@ -545,4 +549,7 @@ def build_knowledge_graph(map_edges, map_nodes, log=False):
 
     # return
     return knowledge_graph
+
+
+
 
