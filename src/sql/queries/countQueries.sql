7e151ff8e4fb82e751057f6079adf9fd4335652c
Marc Duby <mduby@broadinstitute.org>
2026-06-10 14:51:12 -0400

    kgx: added extra count query

--- src/sql/queries/countQueries.sql
+++ src/sql/queries/countQueries.sql
@@ -9,4 +9,17 @@ and source.node_type_id = source_type.type_id and target.node_type_id = target_t
 group by edge_type.type_name, source_type.type_name, target_type.type_name
 order by source_type.type_name, target_type.type_name, edge_type.type_name;
 
+-- count by ontology type
+select count(edge.id) as edge_count, source_type.ontology_name as source_type, target_type.ontology_name as target_type, 
+    edge_type.type_name as edge_type
+from comb_edge_node edge, comb_node_ontology source, comb_node_ontology target, comb_lookup_type edge_type,
+    comb_ontology_type source_type, comb_ontology_type target_type
+where edge.source_node_id = source.id 
+and edge.target_node_id = target.id 
+and source.ontology_type_id = source_type.ontology_id 
+and target.ontology_type_id = target_type.ontology_id 
+and edge.edge_type_id = edge_type.type_id
+group by edge_type.type_name, source_type.ontology_name, target_type.ontology_name
+order by source_type.ontology_name, target_type.ontology_name, edge_type.type_name;
+
 
